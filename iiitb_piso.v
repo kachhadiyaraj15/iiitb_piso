f1e3dfb86aeac6c91427cadedd5ab1b0f6264a75
kachhadiyaraj15 <110079774+kachhadiyaraj15@users.noreply.github.com>
2022-07-27 10:01:24 +0530

    Update iiitb_piso.v

--- iiitb_piso.v
+++ iiitb_piso.v
@@ -1,1 +1,22 @@
-
+module piso (input load, clk, rst,
+             input [7:0] data_in,
+             output reg data_out);
+  
+  // PISO register array to load and shift data
+  reg [7:0] data_reg;
+  
+  always @ (posedge clk or negedge rst) begin
+    if (~rst)
+      data_reg <= 8'h00; // Reset PISO register array on reset
+    else begin
+      
+      // Load the data to the PISO register array and reset the serial data out register
+      if (load)
+      	{data_reg, data_out} <= {data_in, 1'b0};
+      // Shift the loaded data 1 bit right; into the serial data out register
+      else
+      	{data_reg, data_out} <= {1'b0, data_reg[7:0]};
+    end
+  end
+  
+endmodule
